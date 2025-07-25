f0afe86bf191054457ef5d0b41364bbf37a34ece
Gaurav <gauravdhirwani2731@gmail.com>
2025-07-25 18:07:18 +0530

    fix: displayed favorite movies

--- src/contexts/MovieContext.jsx
+++ src/contexts/MovieContext.jsx
@@ -2,7 +2,7 @@ import { createContext, useState, useContext, useEffect } from "react";
 
 const MovieContext = createContext()
 
-export const useMovieContext = () => useContext(MovieContext) 
+export const useMovieContext =  () => useContext(MovieContext) 
 
 export const MovieProvider = ({children}) => {
 
--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -3,23 +3,23 @@ import MovieCard from "../components/MovieCard";
 import { useMovieContext } from "../contexts/MovieContext";
 
 function Favorites() {
-    const {favorites} = useMovieContext();
+    const { favorites } = useMovieContext();
 
     if(favorites) {
-        <div className="favorites">
-            <h2>Your favorites</h2>
-            <div className="movies-grid">   
-                    {favorites.map(
-                        (movie) => 
-                        (
-                            <MovieCard movie={movie} key={movie.id} />
-                        )
-                    )}
+        return (
+            <div className="favorites">
+                <h2>Your favorites</h2>
+                <div className="movies-grid">   
+                        {favorites.map(
+                            (movie) => 
+                            (
+                                <MovieCard movie={movie} key={movie.id} />
+                            )
+                        )}
+                </div>
             </div>
-        </div>
+        );
     }
-
-
     return (
         <div className="favorites-empty">
             <h2>No favorite movies yet</h2>
