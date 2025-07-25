3077f0cdb28d3166792d7940d6c8b6ce680b00fc
Gaurav <gauravdhirwani2731@gmail.com>
2025-07-25 17:59:55 +0530

    feat: implemented favorite/unfavorite logic

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,7 +1,25 @@
 import "../css/Favorites.css"
-
+import MovieCard from "../components/MovieCard";
+import { useMovieContext } from "../contexts/MovieContext";
 
 function Favorites() {
+    const {favorites} = useMovieContext();
+
+    if(favorites) {
+        <div className="favorites">
+            <h2>Your favorites</h2>
+            <div className="movies-grid">   
+                    {favorites.map(
+                        (movie) => 
+                        (
+                            <MovieCard movie={movie} key={movie.id} />
+                        )
+                    )}
+            </div>
+        </div>
+    }
+
+
     return (
         <div className="favorites-empty">
             <h2>No favorite movies yet</h2>
